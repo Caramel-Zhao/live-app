3b5905ffbd8f73eb7422c9ffd41f4db40c513477
SuSu-Lin <1589372895@qq.com>
2019-11-19 14:07:25 +0800

    day1

---
+++ live-app-front/src/components/User/UserContent/Nobility.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name:"Nobility"
+}
+</script>
+
+<style>
+
+</style>
--- live-app-front/src/views/User/User.vue
+++ live-app-front/src/views/User/User.vue
@@ -1,10 +1,22 @@
 <template>
-  <h1>user page</h1>
+    <div>
+      <User-header></User-header>
+      <User-personal></User-personal>
+      <User-content></User-content>
+    </div>
 </template>
 
 <script>
+import header from "../../components/User/UserHeader"
+import personal from "../../components/User/UserPersonal"
+import content from "../../components/User/UserContent"
 export default {
-  name: "User"
+  name: "User",
+  components:{
+    "User-header":header,
+    "User-personal":personal,
+    "User-content":content
+  }
 }
 </script>
 
