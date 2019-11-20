5662b7021a08a5d9b6b395d9de2c6ec36512c161
Gx-web <19040455204@qq.com>
2019-11-20 10:40:08 +0800

    Gx-update

---
+++ live-app-front/src/components/User/data/Signature.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="Signature">
+        <div class="Signature-title">签名</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Signature"
+    }
+</script>
+
+<style scoped>
+    .Signature{
+        width: 100%;
+        height: 0.6rem;
+        background: white;
+        margin-bottom: 0.01rem;
+    }
+    .Signature-title{
+        width: 0.65rem;
+        height: 100%;
+        font-size: 0.18rem;
+        text-align: center;
+        line-height: 0.6rem;
+    }
+</style>
