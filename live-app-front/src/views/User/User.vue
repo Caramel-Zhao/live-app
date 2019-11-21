5f1bca7d5e07c41711c3372384beadcb6ec5c5bf
Caramel <34457500@qq.com>
2019-11-21 10:29:14 +0800

    clear merge

--- live-app-front/src/views/User/User.vue
+++ live-app-front/src/views/User/User.vue
@@ -14,12 +14,6 @@
     <User-Manage></User-Manage>
     <User-Service></User-Service>
     <Footer />
-    <!-- <div class="footer">
-      <span @click="goHome"><i></i>首页</span>
-      <span class="live-img"><img src="../../assets/HomeImg/live.png"
-             alt=""></span>
-      <span><i></i>我的</span>
-    </div> -->
   </div>
 </template>
 
@@ -68,28 +62,5 @@ export default {
 .user {
   width: 100%;
   height: 8rem;
-}
-.footer {
-  font-size: 0.12rem;
-  height: 0.4rem;
-  border-top: 0.005rem solid #eee;
-  position: fixed;
-  bottom: 0rem;
-  display: flex;
-  justify-content: space-around;
-  width: 100%;
-  line-height: 0.4rem;
-  background-color: white;
-  .live-img {
-    display: inline-block;
-    width: 0.9rem;
-    height: 0.9rem;
-    position: relative;
-    top: -0.3rem;
-    img {
-      width: 100%;
-      height: 100%;
-    }
-  }
 }
 </style>
