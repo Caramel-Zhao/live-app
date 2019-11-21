799e4a31591d1010626f95a29780c344fe03d67d
Caramel <34457500@qq.com>
2019-11-21 09:52:43 +0800

    commit1

--- live-app-front/src/views/Home/Home.vue
+++ live-app-front/src/views/Home/Home.vue
@@ -1,41 +1,42 @@
 <template>
   <div>
-    <home-header v-if="HomeInfo" :data="HomeInfo"></home-header>
-    <home-item v-if="HomeInfo" :data="HomeInfo"></home-item>
+    <home-header v-if="HomeInfo"
+                 :data="HomeInfo"></home-header>
+    <home-item v-if="HomeInfo"
+               :data="HomeInfo"></home-item>
     <YT-footer></YT-footer>
   </div>
 </template>
 
 <script>
-  import HomeItem from "../../components/Home/HomeItem";
-  import HomeApi from  '../../apis/Home/home'
-  import HomeHeader from "../../components/Home/HomeHeader";
+import HomeItem from "../../components/Home/HomeItem";
+import HomeApi from '../../apis/Home/home'
+import HomeHeader from "../../components/Home/HomeHeader";
 import Footer from "../../components/Commom/Footer";
 export default {
   name: "Home",
   components: {
-   [Footer.name]: Footer,
-    [HomeHeader.name]:HomeHeader,
-    [HomeItem.name]:HomeItem
+    [Footer.name]: Footer,
+    [HomeHeader.name]: HomeHeader,
+    [HomeItem.name]: HomeItem
 
   },
-  data(){
+  data () {
     return {
-      HomeInfo:null
+      HomeInfo: null
     }
   },
-  methods:{
-    async _initHomeDAta(){
+  methods: {
+    async _initHomeDAta () {
       this.HomeInfo = await HomeApi.getHomeInfo();
       console.log(this.HomeInfo)
     }
   },
-  mounted() {
+  mounted () {
     this._initHomeDAta()
   }
 }
 </script>
 
 <style scoped lang="scss">
-
 </style>
