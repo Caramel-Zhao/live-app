4a2a45763f6241ea707e43bcfb3197b51bb84eaf
Da-Hu <2464665306@qq.com>
2019-11-29 15:45:18 +0800

    xue

---
+++ live-app-front/src/apis/Nobility.js
@@ -0,0 +1,16 @@
+export default {
+    async getvip (userid,vip) {
+      let res = await fetch('http://39.98.126.184:8080/api/getvip/', {
+          method:"POST",
+          headers:{
+              "Content-Type":"application/json;charset:UTF-8"
+          },
+          body:JSON.stringify({
+              "userid":userid,
+              "getvip":vip
+          })
+      })
+      return await res.json()
+    },
+  
+  }
